a4ec5286c9f1696535f7e2a85429cad7cacb07cb
mysterious64 <mvale080@fiu.edu>
2017-01-19 16:38:49 -0500

    mroe chip stuff

--- src/Chip.java
+++ src/Chip.java
@@ -1,13 +1,92 @@
 
 public class Chip {
 	
+	private String name;
+	private char[] frames = new char[2];
+	private double multiplier;
+	private char type; //p = Photon Arts or Techniques, C = Character, w = weaponoid, e = enemy, i = intensifier/booster
+	private char element; // f = fire, i = ice, e = lightning(electric), w = wind, l = light, d = dark
+	
 	public Chip()
 	{
-		String name;
-		char frame;
-		double multiplier;
-		char type;
-		char element;
+		name = "";
+		frames[0] = '\0';
+		frames[1] = '\0';
+		multiplier = 0;
+		type = '\0';
+		element = '\0';
+	}
+	/*
+	public Chip (String n, char f, double m, char t, char e)
+	{
+		name = n;
+		frames[0] = f;
+		frames[1] = '\0';
+		multiplier = m;
+		type = t;
+		element = e;
+	} 
+	*/
+
+	public Chip (String n, char f1, char f2, double m, char t, char e)
+	{
+		name = n;
+		frames[0] = f1;
+		frames[1] = f2;
+		multiplier = m;
+		type = t;
+		element = e;
+	}
+	
+	public void setName(String n)
+	{
+		name = n;
+	}
+	
+	public void setFrame (char f1, char f2)
+	{
+		frames[0] = f1;
+		frames[1] = f2;
+	}
+	
+	public void setMult (double m)
+	{
+		multiplier = m;
+	}
+	
+	public void setType (char t)
+	{
+		type = t;
+	}
+	
+	public void setElement (char e)
+	{
+		element = e;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public char[] getFrame()
+	{
+		return frames;
+	}
+	
+	public double getMult()
+	{
+		return multiplier;
+	}
+	
+	public char getType()
+	{
+		return type;
+	}
+	
+	public char getElement()
+	{
+		return element;
 	}
 	
 	
