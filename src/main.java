47fe802dc69ba8b45d35877ba4acea0a8e9430f1
mysterious64 <mvale080@fiu.edu>
2017-01-20 13:52:54 -0500

    a picture and other changes

--- src/main.java
+++ src/main.java
@@ -1,8 +1,19 @@
 
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Chip chip = new Chip();
+		chip.setName("Sylph Malinca");
+		chip.setFrame('e', '\0');
+		chip.setMult(85);
+		chip.setType('w');
+		System.out.println("New Chip: " + chip.getName());
+		System.out.println(chip.getName() + " has type " + chip.getType());
+		System.out.println(chip.getName() + " has magnitude " + chip.getMult());
+		
 		
 
 	}
